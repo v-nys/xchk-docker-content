bacd2546fa1d8ed3d0471929ffc18bb26556509b
Vincent Nys <vincentnys@gmail.com>
2020-08-16 11:00:49 +0200

    Skelet van wat nodig is om MySQL container te gebruiken.

--- xchk_docker_content/contentviews.py
+++ xchk_docker_content/contentviews.py
@@ -1,9 +1,48 @@
 from xchk_core.contentviews import ContentView
 from xchk_core.strats import *
 
+# iets over hub?
+# iets over play with docker?
+# https://www.docker.com/resources/what-container
+
+class DockerRunIntroView(ContentView):
+     
+    uid = 'docker_run_intro_1'
+    template = 'xchk_docker_content/docker_run_intro.html'
+    title = 'Je eerste container opstarten: docker run'
+    strat = Strategy(refusing_check=TrueCheck(),
+                     accepting_check=Negation(TrueCheck()))
+
+
+class ContainersVSVMsView(ContentView):
+     
+    uid = 'containers_vs_vms_1'
+    template = 'xchk_docker_content/containers_vs_vms.html'
+    title = 'Containers vergeleken met virtuele machines'
+    strat = Strategy(refusing_check=TrueCheck(),
+                     accepting_check=Negation(TrueCheck()))
+
+class WhatAreApplicationContainersView(ContentView):
+     
+    uid = 'what_are_application_containers_1'
+    template = 'xchk_docker_content/what_are_application_containers.html'
+    title = 'Wat zijn applicatiecontainers?'
+    strat = Strategy(refusing_check=TrueCheck(),
+                     accepting_check=Negation(TrueCheck()))
+
 class WhatIsDockerView(ContentView):
      
-    uid = 'what_is_docker'
+    uid = 'what_is_docker_1'
     template = 'xchk_docker_content/what_is_docker.html'
+    title = 'Wat is Docker?'
+    strat = Strategy(refusing_check=TrueCheck(),
+                     accepting_check=Negation(TrueCheck()))
+
+class DockerDesktopInstallationView(ContentView):
+     
+    uid = 'docker_desktop_installation_1'
+    template = 'xchk_docker_content/docker_desktop_installation.html'
+    title = 'Docker Desktop installeren'
+    # TODO: testen dat installatie gelukt is via bv. `docker --version`,...
     strat = Strategy(refusing_check=TrueCheck(),
                      accepting_check=Negation(TrueCheck()))
--- xchk_docker_content/course.py
+++ xchk_docker_content/course.py
@@ -4,4 +4,8 @@ from .contentviews import *
 
 course = Course('docker',
                 'Docker',
-                [(WhatIsDockerView,[])])
+                [(DockerRunIntroView,[DockerInstallationView]),
+                 (DockerDesktopInstallationView,[WhatIsDockerView]),
+                 (ContainersVSVMsView,[WhatIsDockerView]),
+                 (WhatIsDockerView,[WhatAreApplicationContainersView]),
+                 (WhatAreApplicationContainersView,[])])
